6dc80b067df0f3fdbd1a3167c8c0ddbe664ec524
Louis Liu <louis@Louis-MacBook-Air.local>
2025-11-20 16:53:50 +0000

    more sampling

--- Project/1D_harmonic_oscillator.py
+++ Project/1D_harmonic_oscillator.py
@@ -4,9 +4,10 @@ Louis Liu 19/11
 """
 
 import numpy as np
+from function_sampling import metropolis_hastings
 
 
-def eigenfunctions(x: np.ndarray | float, n: int) -> tuple:
+def eigenfunctions(n: int) -> tuple:
     """
     Compute the nth eigenfunction of the 1D harmonic oscillator at position x.
     Parameters:
@@ -15,24 +16,50 @@ def eigenfunctions(x: np.ndarray | float, n: int) -> tuple:
     Returns:
         tuple: The value(s) of the nth eigenfunction at x, and the dimensionless energy eigenvalue.
     """
-    H = 0
-    if n == 0:
-        H = 1.0
 
-    if n == 1:
-        H = 2 * x
+    def f(x):
+        x = np.asarray(x)
+        H = 0
+        if n == 0:
+            H = 1.0
 
-    h_prev = 1.0  # H_0
-    h_curr = 2 * x  # H_1
+        if n == 1:
+            H = 2 * x
 
-    for i in range(1, n):
-        h_next = (2 * x * h_curr) - (2 * i * h_prev)
-        h_prev = h_curr
-        h_curr = h_next
+        h_prev = 1.0  # H_0
+        h_curr = 2 * x  # H_1
 
-    H = h_curr
-    return np.exp(-x**2 / 2) * H, n + 0.5
+        for i in range(1, n):
+            h_next = (2 * x * h_curr) - (2 * i * h_prev)
+            h_prev = h_curr
+            h_curr = h_next
+
+        H = h_curr
+        return np.exp(-x**2 / 2) * H
+
+    return f, n + 0.5
 
 
 def test():
-    f, _ = eigenfunctions(np.array([0.0]), 0)
+    x = np.linspace(0, 10, 100)
+    N = 100000
+    f, _ = eigenfunctions(4)
+    samples = metropolis_hastings(lambda x: f(x)**2, 'gaussian', [0.], xmin=[0.], xmax=[10.], N=N, kwrgs={
+                                  'sigma': 1}).flatten()
+
+    #  discard first 10% of samples as burn-in
+    samples = samples[int(0.1 * N):]
+
+    import matplotlib.pyplot as plt
+    plt.hist(samples, bins=500, density=True,
+             alpha=0.6, label='Sampled Distribution')
+
+    pdf = f(x)**2
+    pdf /= np.trapz(pdf, x)
+    plt.plot(x, pdf, label='Target Distribution', color='red')
+    plt.legend()
+    plt.show()
+
+
+if __name__ == "__main__":
+    test()
--- Project/function_sampling.py
+++ Project/function_sampling.py
@@ -3,23 +3,48 @@ A module to sample functions from a given function space.
 Louis Liu 19/11
 """
 import numpy as np
+from typing import Callable
 
 
-def metropolis_hastings(f: function, f_prop: function, x_0: np.ndarray, N: int = 10000, kwrgs: dict = None):
+class ProposalFunction:
+    @staticmethod
+    def gaussian(x: np.ndarray | float, kwrgs: dict) -> float:
+        """
+        Gaussian proposal function to generate new samples.
+        Parameters:
+            x : float, The current position.
+            kwrgs : dict, Additional keyword arguments containing 'sigma'.
+        Returns:
+            float: The proposed new position.
+        """
+        sigma = kwrgs.get('sigma', 1.0)
+        return np.random.normal(loc=x, scale=sigma)
+
+
+def metropolis_hastings(f: Callable, f_prop: str, x_0: np.ndarray, xmin: np.ndarray, xmax: np.ndarray, N: int = 10000, kwrgs: dict = None) -> np.ndarray:
     """
     Metropolis algorithm to sample from a target distribution defined by nd function f.
 
     Parameters:
         f : callable, The target function to sample from.
-        f_prop : callable, The proposal function to generate new samples.
+        f_prop : str, the proposal function for the distribution.
+        xmin : list or np.ndarray, The minimum bounds for each dimension.
+        xmax : list or np.ndarray, The maximum bounds for each dimension.
         x_0 : list or np.ndarray, The initial position to start sampling from.
         N : int, The number of samples to generate.
         kwrgs : dict, Additional keyword arguments to pass to the proposal function.
 
     Returns: np.ndarray, An array of sampled positions.
     """
+    if f_prop == 'gaussian':
+        f_prop = ProposalFunction.gaussian
+    else:
+        raise ValueError(f"Unknown proposal function '{f_prop}'")
+
+    current = np.array(x_0, dtype=float)
+    xmin = np.array(xmin, dtype=float)
+    xmax = np.array(xmax, dtype=float)
     samples = []
-    current = x_0
     samples.append(current)
 
     accepted_count = 0
@@ -33,10 +58,18 @@ def metropolis_hastings(f: function, f_prop: function, x_0: np.ndarray, N: int =
         # greater than 1 means a higher probability
         acceptance_ratio = prob_proposal / prob_current
 
+        if np.any(proposal < xmin) or np.any(proposal > xmax):
+            samples.append(current.copy())
+            continue
+
         if np.random.uniform(0, 1) < min(1, acceptance_ratio):
             current = proposal
             accepted_count += 1
         samples.append(current)
 
     # print(f"Acceptance Rate: {accepted_count / N:.2f}")
-    return np.array(samples)
+    return np.vstack(samples)
+
+
+def MALA():
+    pass
