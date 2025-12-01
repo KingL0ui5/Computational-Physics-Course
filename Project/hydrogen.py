trying to fix numerical problems

Project/hydrogen.py
@@ -7,16 +7,14 @@ import numpy as np
 from modules.function_sampling import metropolis_hastings
 from modules.differentiators import double_central_difference
 from modules.helpers import hydrogen
-from modules.minimisers import hydrogen_adapted_gradient_desecent
+from modules.minimisers import hydrogen_adapted_gradient_desecent, hydrogen_adapted_stochastic_GD, hydrogen_adapted_quasi_newton
 eps = 1e-15
 
 
 class hydrogen_wavefunction:
     def __init__(self, theta: float):
-        self.f = hydrogen.anstatz(theta)
+        self._f = hydrogen.anstatz(theta)
         self._theta = theta
-        if theta <= 0:
-            print("!!! ERROR: Theta is negative or zero! This will crash. !!!")
 
     def psi(self, coords: np.ndarray) -> np.ndarray | float:
         """
@@ -26,7 +24,7 @@ class hydrogen_wavefunction:
         Returns:
             np.ndarray | float: The value of the trial wavefunction at the given position(s)
         """
-        return self.f(coords)
+        return self._f(coords)
 
     def probability_density(self, coords: np.ndarray) -> np.ndarray | float:
         """
@@ -36,7 +34,7 @@ class hydrogen_wavefunction:
         Returns:
             np.ndarray | float: The probability density at the given position(s)
         """
-        return np.abs(self.f(coords))**2
+        return np.abs(self._f(coords))**2
 
     def theta(self) -> float:
         """
@@ -54,15 +52,21 @@ class hydrogen_wavefunction:
         Returns:
             np.ndarray: The local energy at the given position(s)
         """
-        coords = np.asarray(coords)
+        stepsize = 1e-5
+        coords = np.asarray(coords, dtype=float)
         if len(coords.shape) == 1:
             coords = np.array([coords])
 
         x, y, z = coords[:, 0], coords[:, 1], coords[:, 2]
         r = np.sqrt(x**2 + y**2 + z**2)
 
+        #  mask rs that are too close to 0 - causing numerical problems
+        size_mask = np.abs(r) < stepsize
+        safe_signs = np.where(r != 0, np.sign(r), 1.0)
+        r[size_mask] = safe_signs[size_mask] * stepsize
+
         d2psi = double_central_difference(
-            self.psi, coords, h=[1e-5, 1e-5, 1e-5], order=8)
+            self.psi, coords, h=[stepsize, stepsize, stepsize], order=8)
         #  the problem is here in the H_partial theta as well.
         #  numerical instability ^^^ consider ways to mask this instability or otherwise
 
@@ -83,13 +87,13 @@ def test_derivative(wf, samples):
 def find_minima():
     #  gradient function in terms of theta
     def hydrogen_energy_grad(theta, samples):
-        wf = hydrogen_wavefunction(float(theta))
+        wf = hydrogen_wavefunction(theta)
         return hydrogen.H_partial_theta(wf, samples, analytic=True)
 
-    theta0 = 1.
+    theta0 = 1
 
     theta_min, E_min = hydrogen_adapted_gradient_desecent(
-        hydrogen_wavefunction, hydrogen_energy_grad, x_0=theta0, stepsize=1e-5, detail=True, N_s=10000)
+        hydrogen_wavefunction, hydrogen_energy_grad, x_0=theta0, stepsize=1e-5, detail=True, N_s=10000, max_iter=100)
 
     print(f"minimum value of theta: {theta_min} \nminimum energy: {E_min}")
 

Project/modules/minimisers.py
@@ -8,7 +8,97 @@ import numpy as np
 import time
 
 
-def hydrogen_adapted_gradient_desecent(wf: Callable, dH: Callable, x_0: np.ndarray, stepsize: float, max_iter: int = 10000, stop_tol: float = 1e-6, N_s: int = 1000, detail: bool = False) -> tuple:
+def hydrogen_adapted_quasi_newton(wf: Callable, dH: Callable, x_0: np.ndarray, stepsize: float, method: str = "DFP", max_iter: int = 1000, N_s: int = 1000, stop_tol: float = None, detail: bool = False) -> tuple:
+    """
+    A method to find the minima of a function using the quasi-newton method using a chosen method to approximate the hessian
+    Parameters:
+        f: callable, the function to minimise 
+        df: callable, the first derivative of the function, left for flexibility of method 
+        x_0: np.ndarray, the starting point of the minimiser
+        stepsize: float, the stepsize of the minimiser
+        method: str, default = "DFP" the method to use for the hessian approximation - allowed values "DFP", "BFGS"
+        stop_tol: float, default = None, the value of the gradient at which convergence is determined. defaults to none to give a chance to get off local minima
+        max_iter: int, default = 10000 the number of iterations to run
+        detail: bool, default = False, show the time elapsed for the method to run
+
+    Returns:
+        tuple: the coordinates of minima, the minimum value of the function at this point
+    """
+    from modules.function_sampling import metropolis_hastings
+    x = np.array(x_0, dtype=float)
+    if x.ndim == 0:
+        x = x.reshape(1)
+    n_dim = len(x)
+    G = np.eye(n_dim)
+
+    def DFP(x_new, grad_new, x, grad, G):
+        delta = x_new - x
+        gamma = grad_new - grad
+
+        if np.dot(gamma, delta) == 0:
+            return G
+
+        G = G + np.outer(delta, delta) / np.dot(gamma, delta) - \
+            (G @ np.outer(gamma, gamma) @ G) / np.dot(gamma, G @ gamma)
+        return G
+
+    def BFGS(x_new, grad_new, x, grad, G):
+        delta = x_new - x
+        gamma = grad_new - grad
+        dim = len(delta)
+
+        temp = 1.0 / np.dot(gamma, delta)
+        I = np.eye(dim)
+        G = (I - temp * np.outer(delta, gamma)) @ G @ (I - temp * np.outer(gamma, delta)) \
+            + temp * np.outer(delta, delta)
+
+        return G
+
+    if method == "DFP":
+        grad_update = DFP
+
+    elif method == "BFGS":
+        grad_update = BFGS
+
+    start = time.perf_counter()
+    for i in range(max_iter):
+        psi = wf(theta=x[0])  # hydrogen wavefunction given theta
+        samples = metropolis_hastings(f=psi.probability_density, f_prop='gaussian', x_0=[
+                                      1., 1., 1.], xmin=[-10., -10., -10.], xmax=[10., 10., 10.], N=N_s, kwrgs={'sigma': 2.})
+        samples = samples[N_s//10:]
+
+        grad = np.asarray(dH(x, samples), dtype=float)
+
+        if grad.ndim == 0:
+            grad = grad.reshape(1)
+
+        if stop_tol:
+            if np.linalg.norm(grad) < stop_tol:
+                print("halting QN")
+                break
+
+        x_new = x - stepsize * G @ grad
+        psi_new = wf(theta=x_new[0])  # hydrogen wavefunction given theta
+        samples_new = metropolis_hastings(f=psi_new.probability_density, f_prop='gaussian', x_0=[
+            1., 1., 1.], xmin=[-10., -10., -10.], xmax=[10., 10., 10.], N=N_s, kwrgs={'sigma': 2.})
+        samples_new = samples_new[N_s//10:]
+
+        grad_new = np.asarray(dH(x_new, samples_new), dtype=float)
+        if grad_new.ndim == 0:
+            grad_new = grad_new.reshape(1)
+
+        G = grad_update(x_new, grad_new, x, grad, G)
+        x = x_new
+    end = time.perf_counter()
+    time_elapsed = end - start
+
+    if detail:
+        print(f"iteration number QN: {i}")
+        print(f"time elapsed QN: {time_elapsed}")
+    return x, np.nanmean(psi.local_energy(coords=samples))
+
+
+def hydrogen_adapted_gradient_desecent(wf: Callable, dH: Callable, x_0: np.ndarray, stepsize: float, max_iter: int = 1000, stop_tol: float = 1e-6, N_s: int = 1000, detail: bool = False) -> tuple:
     """
     A method to find the minima of a function using the gradient descent method.
     Parameters:
@@ -41,6 +131,9 @@ def hydrogen_adapted_gradient_desecent(wf: Callable, dH: Callable, x_0: np.ndarr
             break
 
         x = x - stepsize * d
+        #  restrict x to be positive
+        if x <= 0.001:
+            x = 0.001
 
     end = time.perf_counter()
     time_elapsed = end - start
@@ -52,7 +145,57 @@ def hydrogen_adapted_gradient_desecent(wf: Callable, dH: Callable, x_0: np.ndarr
     return x, np.nanmean(psi.local_energy(coords=samples))
 
 
-def gradient_desecent(f: Callable, df: Callable, x_0: np.ndarray, stepsize: float, max_iter: int = 10000, stop_tol: float = 1e-6, detail: bool = False, **kwargs) -> tuple:
+def hydrogen_adapted_stochastic_GD(wf: Callable, dH: Callable, x_0: np.ndarray, stepsize: float, stop_tol: float = None, max_iter: int = 1000, noise: float = 0.1, N_s: int = 1000, detail: bool = False) -> tuple:
+    """
+    A method to find the minima of a function using the gradient descent method with added noise. 
+    Parameters:
+        E: callable, the function to minimise
+        dH: callable, the first derivative of the function, left for flexibility of method
+        x_0: np.ndarray, the starting point of the minimiser
+        stepsize: float, the stepsize of the minimizer
+        max_iter: int, the number of iterations to run
+        noise: float, the level of noise to introduce to the derivative
+        stop_tol: float, default = 1e-6, the value of the gradient at which convergence is determined
+        N_s: number of samples to take per wavefunction iteration
+        detail: bool, default = False, show the time elapsed for the method to run
+
+    Returns:
+        tuple: the coordinates of minima, the minimum value of the function at this point
+    """
+    from modules.function_sampling import metropolis_hastings
+
+    x = np.asarray(x_0, dtype=float)
+    start = time.perf_counter()
+
+    for i in range(max_iter):
+        psi = wf(theta=x)  # hydrogen wavefunction given theta
+        samples = metropolis_hastings(f=psi.probability_density, f_prop='gaussian', x_0=[
+                                      1., 1., 1.], xmin=[-10., -10., -10.], xmax=[10., 10., 10.], N=N_s, kwrgs={'sigma': 2.})
+        samples = samples[N_s//10:]
+
+        d = dH(x, samples)
+
+        #  stopping conditions
+        if stop_tol:
+            if np.linalg.norm(d) < stop_tol:
+                break
+
+        sigma = noise * stepsize
+        d += np.random.normal(loc=0, scale=sigma, size=x.shape)
+        x = x - stepsize * d
+
+    end = time.perf_counter()
+    time_elapsed = end - start
+
+    if detail:
+        print(f"iteration number SGD: {i}")
+        print(f"time elapsed SGD: {time_elapsed}")
+        print(f"final derivative: {d}")
+
+    return x, np.nanmean(psi.local_energy(coords=samples))
+
+
+def gradient_desecent(f: Callable, df: Callable, x_0: np.ndarray, stepsize: float, max_iter: int = 1000, stop_tol: float = 1e-6, detail: bool = False, **kwargs) -> tuple:
     """
     A method to find the minima of a function using the gradient descent method. 
     Parameters:
@@ -85,7 +228,7 @@ def gradient_desecent(f: Callable, df: Callable, x_0: np.ndarray, stepsize: floa
     return x, f(x, **kwargs)
 
 
-def stochastic_GD(f: Callable, df: Callable, x_0: np.ndarray, stepsize: float, noise: float, max_iter: int = 10000, stop_tol: float = None, detail: bool = False, **kwargs) -> tuple:
+def stochastic_GD(f: Callable, df: Callable, x_0: np.ndarray, stepsize: float, noise: float, max_iter: int = 1000, stop_tol: float = None, detail: bool = False, **kwargs) -> tuple:
     """
     A method to find the minima of a function using the gradient descent method with added noise. 
     Parameters:
@@ -120,7 +263,7 @@ def stochastic_GD(f: Callable, df: Callable, x_0: np.ndarray, stepsize: float, n
     return x, f(x, **kwargs)
 
 
-def RMSProp_GD(f: Callable, df: Callable, x_0: np.ndarray, stepsize: float, forgetting: float, max_iter: int = 10000, stop_tol: float = 1e-6, detail: bool = False, **kwargs) -> tuple:
+def RMSProp_GD(f: Callable, df: Callable, x_0: np.ndarray, stepsize: float, forgetting: float, max_iter: int = 1000, stop_tol: float = 1e-6, detail: bool = False, **kwargs) -> tuple:
     """
     A method to find the minima of a function using the RMSProp adjusted gradient descent method. 
     Parameters:
@@ -156,7 +299,7 @@ def RMSProp_GD(f: Callable, df: Callable, x_0: np.ndarray, stepsize: float, forg
     return x, f(x, **kwargs)
 
 
-def quasi_newton(f: Callable, df: Callable, x_0: np.ndarray, stepsize: float, method: str = "DFP", max_iter: int = 10000, stop_tol: float = None, detail: bool = False, **kwargs) -> tuple:
+def quasi_newton(f: Callable, df: Callable, x_0: np.ndarray, stepsize: float, method: str = "DFP", max_iter: int = 1000, stop_tol: float = None, detail: bool = False, **kwargs) -> tuple:
     """
     A method to find the minima of a function using the quasi-newton method using a chosen method to approximate the hessian
     Parameters:
@@ -179,6 +322,9 @@ def quasi_newton(f: Callable, df: Callable, x_0: np.ndarray, stepsize: float, me
     def DFP(x_new, grad_new, x, grad, G):
         delta = x_new - x
         gamma = grad_new - grad
+
+        if np.dot(gamma, delta) == 0:
+            return G
 
         G = G + np.outer(delta, delta) / np.dot(gamma, delta) - \
             (G @ np.outer(gamma, gamma) @ G) / np.dot(gamma, G @ gamma)